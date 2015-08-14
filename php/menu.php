c3a97e57cdae80f605c4ff6a81863518e09d8fa1
Conor Gilmer <conor.gilmer@gmail.com>
2015-08-14 18:39:46 +0100

    seedings

---
+++ php/menu.php
@@ -0,0 +1,30 @@
+  </head>
+
+  <body>
+    <div class="container">
+
+      <!-- Static navbar -->
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">World Cup Qualification Groups</a>
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            	<ul class="nav navbar-nav">
+            		<li><a href="index.php">Home</a></li>
+
+	    	</ul>
+		<ul class="nav navbar-nav navbar-right">
+			<li><a href="about.php">About</a></li>
+		</ul>
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </nav>
+
+
