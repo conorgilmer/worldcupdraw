b6d046cb559d29592d0f4497fa1b5a81a8d997dc
Conor Gilmer <conor.gilmer@gmail.com>
2015-08-15 20:04:51 +0100

    add colours and left right

--- php/getbounces.php
+++ php/getbounces.php
@@ -1,9 +1,9 @@
 <?php
    $group=$_GET["q"];
-
+   $colours = array("#0000FF","#FF00AA","#00FF00","#3300AA","#FF0000","#333222","#342000","#1110FF","#222222");
  $file = "seedings.csv";
  $c=0;
-
+ $col=0;
   echo "{ \"cols\": [ {\"id\":\"\",\"label\":\"Country\",\"pattern\":\"\",\"type\":\"string\"}, {\"id\":\"\",\"label\":\"Position\",\"pattern\":\"\",\"type\":\"number\"} , {\"id\":\"\",\"role\":\"style\",\"type\":\"string\"} ], \"rows\": [ ";
    
 $data=fopen($file,'r');
@@ -12,8 +12,10 @@ while($row=fgets($data)){
     if($c!=0){
     //do something with it
     $lst = explode(",", $row);
-        if ($lst[5]== $group){ 
-      echo "{\"c\":[{\"v\":\"" . $lst[2] . "\",\"f\":null},{\"v\":" . $lst[3] . ",\"f\":null},{\"v\":\"#0000FF\",\"f\":null} ]},";
+        if ($lst[5]== $group){
+	$pos = ($lst[0] - (9 * $col)) - 4.5; 
+      echo "{\"c\":[{\"v\":\"" . $lst[2] . "\",\"f\":null},{\"v\":" . $pos . ",\"f\":null},{\"v\":\"".$colours[$col]."\",\"f\":null} ]},";
+	$col++;
     } else {
 //      echo "{\"c\":[{\"v\":\"" . 'others' . "\",\"f\":null},{\"v\":" . $row['others'] . ",\"f\":null}]}, ";
     }
